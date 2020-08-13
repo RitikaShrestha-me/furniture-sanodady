82ee057ede6cb3ee1d6d5a3532916e1b806a5c5a
Ritika8189 <shrestharitika189@gmail.com>
2020-08-13 14:09:13 +0530

    initial commit

---
+++ home.js
@@ -0,0 +1,16 @@
+var i = 0;
+var images = [];
+var time;
+
+//list of images
+
+images[0]="";
+images[1]="";
+images[2]="";
+images[3]="";
+
+//slideshow
+
+function changeImg(){
+	document.slide.
+}
